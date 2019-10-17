d537db475a2f3dca6487f3159e7f63f42fe266cc
Jahaziel <jahaziel.vaz@gmail.com>
2019-10-17 09:08:27 -0600

    pust request is updated

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 let express = require('express');
 let app = express();
 app.use(express.static(__dirname));
+let bodyParser = require('body-parser');
+app.use(bodyParser.json())
+
+let urlencoded = bodyParser.urlencoded({extended: false})
 
 let products = [
   {id: 1, name: 'MacBook Pro'},
-  {id: 1, name: 'USB Port'},
-  {id: 1, name: 'Kali Linux'}
+  {id: 2, name: 'USB Port'},
+  {id: 3, name: 'Kali Linux'}
 
 ]
 
-let currentId = 2;
+let currentId = 3;
 
 // Routes
 app.get('/products', (req, res) => {
   res.send({products: products})
+})
+
+app.post('/products', (req, res) => {
+  let productName = req.body.name
+  currentId++
+
+  products.push({
+    id: currentId,
+    name: productName
+  })
+
+  res.send('SUCCESS')
 })
 
 // Server
--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ $(() => {
   $("#get-button").on('click', () => {
     $.ajax({
       url: '/products',
+      type: 'get',
       contentType: 'application/json',
       success: (res) => {
         let tbodyEl = $('tbody');
@@ -22,6 +23,26 @@ $(() => {
         })
       }
     })
+  })
+
+  // CREATE/POST
+  //NOTE: The reason for the prevent default is to prevent the form from refreshing the page, since with jQuery, the page doesn't need to refresh, in order for us to get the results
+  $('#form1').on('submit', (event) => {
+    event.preventDefault()
+  })
+
+  let createInput = $('#create-input');
+
+  $.ajax({
+    url: '/products',
+    method: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify({name: createInput.val()}),
+    success: (res) => {
+      console.log(res)
+      createInput.val('')
+      $('#get-button').click()
+    }
   })
 
 
